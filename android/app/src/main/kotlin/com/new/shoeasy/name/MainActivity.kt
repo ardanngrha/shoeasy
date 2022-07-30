e1a08b96fc9b29174faed3a3ab7c02ad1f75ac1f
adanngrha <contact.ardana@gmail.com>
2022-07-30 09:02:08 +0700

    fixing bug 'name package error' on debugging

--- android/app/src/main/kotlin/com/new/shoeasy/name/MainActivity.kt
+++ android/app/src/main/kotlin/com/new/shoeasy/name/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.new.shoeasy.name
+package com.shoeasy.name
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,12 +1,43 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:shoeasy/theme.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
   const ProductPage({Key? key}) : super(key: key);
 
   @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  final List images = [
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+    'assets/image_shoes.png'
+  ];
+
+  int currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
+    // indicator
+    Widget indicator(int index) {
+      return Container(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 2,
+        ),
+        width: currentIndex == index ? 16 : 4,
+        height: 4,
+        decoration: BoxDecoration(
+          color: currentIndex == index ? primaryColor : const Color(0xffC4C4C4),
+          borderRadius: BorderRadius.circular(10),
+        ),
+      );
+    }
+
     Widget header() {
+      int index = -1;
+
       return Column(
         children: [
           Container(
@@ -32,6 +63,36 @@ class ProductPage extends StatelessWidget {
                 ),
               ],
             ),
+          ),
+          CarouselSlider(
+            items: images
+                .map(
+                  (image) => Image.asset(
+                    image,
+                    width: MediaQuery.of(context).size.width,
+                    height: 310,
+                    fit: BoxFit.cover,
+                  ),
+                )
+                .toList(),
+            options: CarouselOptions(
+              initialPage: 0,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: images.map((e) {
+              index++;
+              return indicator(index);
+            }).toList(),
           ),
         ],
       );
