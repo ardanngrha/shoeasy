9347059ee5d4d04cfc13e656cc1e07de00e51e03
Adan Nugraha <contact.ardana@gmail.com>
2022-08-15 23:22:33 +0700

    addMessage to firestore handler

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shoeasy/models/product_model.dart';
+import 'package:shoeasy/providers/auth_provider.dart';
+import 'package:shoeasy/services/message_service.dart';
 import 'package:shoeasy/theme.dart';
 import 'package:shoeasy/widgets/chat_bubble.dart';
 
@@ -13,180 +16,203 @@ class DetailChatPage extends StatefulWidget {
 }
 
 class _DetailChatPageState extends State<DetailChatPage> {
-  header() {
-    return PreferredSize(
-      preferredSize: const Size.fromHeight(70),
-      child: AppBar(
-        backgroundColor: backgroundColor1,
-        centerTitle: false,
-        title: Row(
+  TextEditingController messageController = TextEditingController(text: '');
+
+  @override
+  Widget build(BuildContext context) {
+    AuthProvider authProvider = Provider.of<AuthProvider>(context);
+
+    handleAddMessage () async {
+      await MessageService().addMessage(
+        user: authProvider.user,
+        isFromUser: true,
+        product: widget.product,
+        message: messageController.text,
+      );
+
+      setState(() {
+        widget.product = UninitializedProductModel();
+        messageController.text = '';
+      });
+    }
+
+    header() {
+      return PreferredSize(
+        preferredSize: const Size.fromHeight(70),
+        child: AppBar(
+          backgroundColor: backgroundColor1,
+          centerTitle: false,
+          title: Row(
+            children: [
+              Image.asset(
+                'assets/image_shop_logo_online.png',
+                width: 50,
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Shoesye Store',
+                    style: primaryTextStyle.copyWith(
+                      fontWeight: medium,
+                      fontSize: 14,
+                    ),
+                  ),
+                  Text(
+                    'Online',
+                    style: secondaryTextStyle.copyWith(
+                      fontWeight: light,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget productPreview() {
+      return Container(
+        width: 225,
+        height: 74,
+        margin: const EdgeInsets.only(
+          bottom: 20,
+        ),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: backgroundColor5,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: primaryColor,
+          ),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset(
-              'assets/image_shop_logo_online.png',
-              width: 50,
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(
+                widget.product.galleries![0].url,
+                width: 54,
+              ),
             ),
             const SizedBox(
-              width: 12,
+              width: 10,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    widget.product.name.toString(),
+                    overflow: TextOverflow.ellipsis,
+                    style: primaryTextStyle,
+                  ),
+                  const SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    '\$${widget.product.price}',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  widget.product = UninitializedProductModel();
+                });
+              },
+              child: Image.asset(
+                'assets/button_close.png',
+                width: 23,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            widget.product is UninitializedProductModel
+                ? const SizedBox()
+                : productPreview(),
+            Row(
               children: [
-                Text(
-                  'Shoesye Store',
-                  style: primaryTextStyle.copyWith(
-                    fontWeight: medium,
-                    fontSize: 14,
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      color: backgroundColor4,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        controller: messageController,
+                        style: primaryTextStyle,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type Message...',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-                Text(
-                  'Online',
-                  style: secondaryTextStyle.copyWith(
-                    fontWeight: light,
-                    fontSize: 14,
+                const SizedBox(
+                  width: 20,
+                ),
+                GestureDetector(
+                  onTap: handleAddMessage,
+                  child: Image.asset(
+                    'assets/button_send.png',
+                    width: 45,
                   ),
                 ),
               ],
             ),
           ],
         ),
-      ),
-    );
-  }
+      );
+    }
 
-  Widget productPreview() {
-    return Container(
-      width: 225,
-      height: 74,
-      margin: const EdgeInsets.only(
-        bottom: 20,
-      ),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: backgroundColor5,
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(
-          color: primaryColor,
+    Widget content() {
+      return ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
         ),
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.network(
-              widget.product.galleries![0].url,
-              width: 54,
-            ),
+        children: const [
+          ChatBubble(
+            isSender: true,
+            text: 'Hi, This item is still available?',
+            hasProduct: true,
           ),
-          const SizedBox(
-            width: 10,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  widget.product.name.toString(),
-                  overflow: TextOverflow.ellipsis,
-                  style: primaryTextStyle,
-                ),
-                const SizedBox(
-                  height: 2,
-                ),
-                Text(
-                  '\$${widget.product.price}',
-                  style: priceTextStyle.copyWith(
-                    fontWeight: medium,
-                  ),
-                )
-              ],
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                widget.product = UninitializedProductModel();
-              });
-            },
-            child: Image.asset(
-              'assets/button_close.png',
-              width: 23,
-            ),
-          ),
+          ChatBubble(
+            isSender: false,
+            text: 'Good night, This item is only available in size 42 and 43',
+            hasProduct: false,
+          )
         ],
-      ),
-    );
-  }
+      );
+    }
 
-  Widget chatInput() {
-    return Container(
-      margin: const EdgeInsets.all(20),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          widget.product is UninitializedProductModel
-              ? const SizedBox()
-              : productPreview(),
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  height: 45,
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                  ),
-                  decoration: BoxDecoration(
-                    color: backgroundColor4,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Center(
-                    child: TextFormField(
-                      decoration: InputDecoration.collapsed(
-                        hintText: 'Type Message...',
-                        hintStyle: subtitleTextStyle,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Image.asset(
-                'assets/button_send.png',
-                width: 45,
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget content() {
-    return ListView(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 20,
-      ),
-      children: const [
-        ChatBubble(
-          isSender: true,
-          text: 'Hi, This item is still available?',
-          hasProduct: true,
-        ),
-        ChatBubble(
-          isSender: false,
-          text: 'Good night, This item is only available in size 42 and 43',
-          hasProduct: false,
-        )
-      ],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: header(),
--- lib/services/message_service.dart
+++ lib/services/message_service.dart
@@ -1,27 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:shoeasy/models/product_model.dart';
 import 'package:shoeasy/models/user_model.dart';
 
 class MessageService {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  Future<void> addMessage(
-      {UserModel? user,
-      bool? isFromUser,
-      String? message,
-      ProductModel? product}) async {
+  Future<void> addMessage({
+    UserModel? user,
+    bool? isFromUser,
+    String? message,
+    ProductModel? product,
+  }) async {
     try {
       firestore.collection('messages').add({
-        'userId': user!.id,
-        'userName': user.name,
-        'userImage': user.profilePhotoUrl,
-        'isFromUser': true,
+        'userId': user?.id,
+        'userName': user?.name,
+        'userImage': user?.profilePhotoUrl,
+        'isFromUser': isFromUser,
         'message': message,
-        'product': product is UninitializedProductModel ? {} : product!.toJson(),
+        'product':
+            product is UninitializedProductModel ? {} : product!.toJson(),
         'createdAt': DateTime.now().toString(),
         'updatedAt': DateTime.now().toString(),
       }).then(
-        (value) => print('Pesan Berhasil Dikirim'),
+        (value) => debugPrint('Pesan Berhasil Dikirim'),
       );
     } catch (e) {
       throw Exception('Pesan Gagal Dikirim!');
